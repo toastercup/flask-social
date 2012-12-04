0b7bd3ab1c84ac63b45e4107cb1eb3a2cf77c9a9
toastercup <alex.tharp@careerbuilder.com>
2012-12-03 22:41:39 -0500

    Prepare for deploy to Heroku

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import app
 from app import db
 
 if __name__ == '__main__':
     db.create_all()
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
