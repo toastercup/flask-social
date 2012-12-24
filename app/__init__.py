5314c2c38639c73504d60c94e1d965537ec19a1d
Toastercup <alex.tharp@careerbuilder.com>
2012-12-24 02:06:30 -0500

    Test 'Access-Control-Allow-Methods' header

--- app/__init__.py
+++ app/__init__.py
@@ -47,5 +47,6 @@ rest.add_resource(NewImageResource,
 def allow_cors(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
 
     return response
